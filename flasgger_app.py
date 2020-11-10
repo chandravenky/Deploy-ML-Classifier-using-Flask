86ae4921b27a24e795a1548d98b19e5cdc98a5a4
chandravenky <venkatesh.chandra@mail.mcgill.ca>
2020-11-09 19:43:42 -0700

    Update flasgger files

--- flasgger_app.py
+++ flasgger_app.py
@@ -9,13 +9,17 @@ import numpy as np
 import pickle
 import pandas as pd
 from flask import Flask, request
-
+import flasgger
+from flasgger import Swagger
 
 #Change working directory - only for debugging purposes
 #import os
-#os.chdir(r'C:\Users\vchan\Documents\Deployed model')
+#os.chdir(r'C:\Users\vchan\Desktop\git\Flassger deployment')
 
 app=Flask(__name__)
+
+#Initiate Swagger app
+Swagger(app)
 
 pickle_in = open("classifier.pkl","rb")
 classifier=pickle.load(pickle_in)
@@ -25,9 +29,32 @@ classifier=pickle.load(pickle_in)
 def welcome():
     return "Risk Classification App"
 
-#---------------Based on values giving-------------
+#---------------Create wrapper and function-------------
 @app.route('/prediction')
 def preditions():
+    
+    """Credit Risk Modelling 
+    Classification model to predict type of risk
+    ---
+    parameters:  
+      - name: age
+        in: query
+        type: number
+        required: true
+      - name: marital_status
+        in: query
+        type: number
+        required: true
+      - name: income
+        in: query
+        type: number
+        required: true
+    responses:
+        200:
+            description: The output values
+        
+    """
+    
     age = request.args.get('age')
     marital_status = request.args.get('marital_status')
     income = request.args.get('income')
@@ -39,6 +66,21 @@ def preditions():
 
 @app.route('/prediction_csv', methods = ["POST"])
 def preditions_csv():
+    
+    """Credit Risk Modelling 
+    Classification model to predict type of risk
+    ---
+    parameters:  
+      - name: file
+        in: formData
+        type: file
+        required: true
+    responses:
+        200:
+            description: The output values
+        
+    """
+    
     df_test = pd.read_csv(request.files.get("file"))
     
     #Generate prediction
